ec88cffc431a5b52becbbf015b708fa664fe1c3a
Jonas Bergström <jontrian97@gmail.com>
2024-04-27 00:00:14 +0200

    Initial Commit

---
+++ FreshGrocery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreshGrocery
+//
+//  Created by Jonas Bergström on 2024-04-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FreshGrocery/FreshGroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshGroceryApp.swift
+//  FreshGrocery
+//
+//  Created by Jonas Bergström on 2024-04-27.
+//
+
+import SwiftUI
+
+@main
+struct FreshGroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
